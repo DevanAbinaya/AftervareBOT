56a6a3250bfea4a621429fdbc90544116edffc7c
DevanAbinaya <dabinaya.fathan@gmail.com>
2022-02-06 19:09:17 +0700

    Create meme.js

---
+++ commands/fun/meme.js
@@ -0,0 +1,35 @@
+const { Message, Client, MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+const ee = require("../../config/embed.json");
+
+module.exports = {
+  name: "meme",
+  description: "See Meme",
+  categories: "fun",
+  aliases: ["mem"],
+  userperm: ['SEND_MESSAGES'],
+  botperm: [],
+  ownerOnly: false,
+  cooldown : 2,
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    const res = await fetch(`https://meme-api.herokuapp.com/gimme`);
+
+    const json = await res.json();
+
+    const Embed = new MessageEmbed()
+      .setColor(ee.embed_color)
+      .setURL(json.postLink)
+      .setTitle(json.title)
+      .setImage(json.url)
+      .setFooter(`👍 ${json.ups || 0} | Author ${json.author || " "}`)
+      .setTimestamp();
+
+    return message.channel.send({ embeds: [Embed] });
+  },
+};
