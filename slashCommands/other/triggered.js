7386e86ece90dd9c7cdc764f415f43df440f7704
DevanAbinaya <dabinaya.fathan@gmail.com>
2022-02-07 23:03:03 +0700

    update

---
+++ slashCommands/other/triggered.js
@@ -0,0 +1,28 @@
+const { Client, CommandInteraction, MessageAttachment } = require('discord.js');
+const Canvas = require('canvas');
+
+module.exports = {
+    name: 'triggered',
+    description: 'Make you triggered',
+    /**
+     * 
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async (client, interaction, args) => {
+        const canvas = Canvas.createCanvas(800, 1000);
+        const context = canvas.getContext('2d');
+        const background = await Canvas.loadImage('./triggered.jpg')
+
+        context.drawImage(background, 0, 0, canvas.width, canvas.height);
+        const userAvatar = await Canvas.loadImage(interaction.user.displayAvatarURL({ format: 'jpg' }));
+        context.drawImage(userAvatar, 0, 0, 800, 800);
+        const sepia = await Canvas.loadImage('./sepia.png');
+        context.drawImage(sepia, 0, 0, 800, 800);
+
+        const finalImage = new MessageAttachment(canvas.toBuffer(), 'finalImage.png');
+
+        interaction.followUp({ content: `**${interaction.user.username} is triggered!!!**`, files: [finalImage] });
+    },
+};
