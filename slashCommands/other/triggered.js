f77d770c1848e0bd182f723cf8f53eef5db43823
DevanAbinaya <dabinaya.fathan@gmail.com>
2022-02-08 13:05:02 +0700

    update

--- slashCommands/other/triggered.js
+++ slashCommands/other/triggered.js
@@ -13,12 +13,12 @@ module.exports = {
     run: async (client, interaction, args) => {
         const canvas = Canvas.createCanvas(800, 1000);
         const context = canvas.getContext('2d');
-        const background = await Canvas.loadImage('./triggered.jpg')
+        const background = await Canvas.loadImage('../../assets/triggered.jpg')
 
         context.drawImage(background, 0, 0, canvas.width, canvas.height);
         const userAvatar = await Canvas.loadImage(interaction.user.displayAvatarURL({ format: 'jpg' }));
         context.drawImage(userAvatar, 0, 0, 800, 800);
-        const sepia = await Canvas.loadImage('./sepia.png');
+        const sepia = await Canvas.loadImage('../../assets/sepia.png');
         context.drawImage(sepia, 0, 0, 800, 800);
 
         const finalImage = new MessageAttachment(canvas.toBuffer(), 'finalImage.png');
