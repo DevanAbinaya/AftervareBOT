437353c734fff0c32fdf3a924f088bfcb2e419fd
DevanAbinaya <dabinaya.fathan@gmail.com>
2022-02-25 22:14:15 +0700

    Update anti-crash.js

--- events/anti-crash.js
+++ events/anti-crash.js
@@ -59,21 +59,21 @@ process.on("uncaughtExceptionMonitor", (err, origin) => {
 
 });
 
-process.on("multipleResolves", (type, promise, reason) => {
-
-    const channel = client.channels.cache.get("936231208659087371")
-
-    const embed = new MessageEmbed()
-    .setAuthor({ name: 'Anti Crash', iconURL: 'https://cdn.discordapp.com/attachments/900382186354671696/929420685015658516/fran-madaraki.png'})
-    .setTitle(`Multiple Resolves`)
-    .setURL("https://nodejs.org/api/process.html#event-multipleresolves")
-    .addField("Type", `\`\`\`${type}\`\`\``, false)
-    .addField("Promise", `\`\`\`${promise}\`\`\``, true)
-    .addField("Reason", `\`\`\`${reason}\`\`\``, true)
-    .setTimestamp()
-    .setFooter({text: "Imagine a bot without anti-crash"})
-    .setColor(`#b20000`)
-
-    return channel.send({ embeds: [embed]})
-
-});
+// process.on("multipleResolves", (type, promise, reason) => {
+// 
+//     const channel = client.channels.cache.get("936231208659087371")
+// 
+//     const embed = new MessageEmbed()
+//     .setAuthor({ name: 'Anti Crash', iconURL: 'https://cdn.discordapp.com/attachments/900382186354671696/929420685015658516/fran-madaraki.png'})
+//     .setTitle(`Multiple Resolves`)
+//     .setURL("https://nodejs.org/api/process.html#event-multipleresolves")
+//     .addField("Type", `\`\`\`${type}\`\`\``, false)
+//     .addField("Promise", `\`\`\`${promise}\`\`\``, true)
+//     .addField("Reason", `\`\`\`${reason}\`\`\``, true)
+//     .setTimestamp()
+//     .setFooter({text: "Imagine a bot without anti-crash"})
+//     .setColor(`#b20000`)
+// 
+//     return channel.send({ embeds: [embed]})
+// 
+// });
