f016093056adac46c82bf3811a68ec7c0c39e75e
DevanAbinaya <97084324+DevanAbinaya@users.noreply.github.com>
2022-04-09 18:46:38 +0700

    Update (schedule)

--- events/other/schedule.js
+++ events/other/schedule.js
@@ -6,69 +6,69 @@ const ms = require('ms');
  // Channel ID (Optional)
 
 client.on("ready", () => {
-    const channel = client.channels.cache.get('927563716403269642')
+    const channel = client.channels.cache.get('752744784174645281')
 
     const embed1 = new MessageEmbed()
     .setColor("WHITE")
     .setTitle("🕌    SAHURRRRRRRR!!     🕌")
-    .setDescription("**Good Morning Y'all !! | ||@everyone|| | SAHUR**\n Hari pertama puasa nih, Yok bangun yok!!\n> Waktu sahurnya masih ada sekitar `2 jam` lagi guys! Jangan sampe telat yaa! :hourglass: \n")
+    .setDescription("**Good Morning Y'all !! | ||<@614645570044166174>|| | SAHUR**\n Udah waktunya sahur nih, Yok bangun yok!!\n> Waktu sahurnya masih ada sekitar `2 jam` lagi, Jangan sampe telat yaa! :hourglass: \n")
     .setFooter({text: `Buruan makan, entar siang laper nangis lagi`, iconURL: `${client.user.displayAvatarURL({dynamic: true, size: 512})}`})
     .setTimestamp();
 
     const embed2 = new MessageEmbed()
     .setColor("WHITE")
     .setTitle("🕌    SAHURRRRRRRR!!     🕌")
-    .setDescription("**Good Morning Y'all !! | ||@everyone|| | SAHUR**\n Hari pertama puasa nih, Yok bangun yok!!\n> Waktu sahurnya masih ada sekitar `1,5 jam` lagi guys! Jangan sampe telat yaa! :hourglass: \n")
+    .setDescription("**Good Morning Y'all !! | ||<@614645570044166174>|| | SAHUR**\n Udah waktunya sahur nih, Yok bangun yok!!\n> Waktu sahurnya masih ada sekitar `1,5 jam` lagi, Jangan sampe telat yaa! :hourglass: \n")
     .setFooter({text: `Buruan makan, entar siang laper nangis lagi`, iconURL: `${client.user.displayAvatarURL({dynamic: true, size: 512})}`})
     .setTimestamp();
     
     const embed3 = new MessageEmbed()
     .setColor("WHITE")
     .setTitle("🕌    SAHURRRRRRRR!!     🕌")
-    .setDescription("**Good Morning Y'all !! | ||@everyone|| | SAHUR**\n Hari pertama puasa nih, Yok bangun yok!!\n> Waktu sahurnya masih ada sekitar `1 jam` lagi guys! Jangan sampe telat yaa! :hourglass: \n")
+    .setDescription("**Good Morning Y'all !! | ||<@614645570044166174>|| | SAHUR**\n Udah waktunya sahur nih, Yok bangun yok!!\n> Waktu sahurnya masih ada sekitar `1 jam` lagi, Jangan sampe telat yaa! :hourglass: \n")
     .setFooter({text: `Buruan makan, entar siang laper nangis lagi`, iconURL: `${client.user.displayAvatarURL({dynamic: true, size: 512})}`})
     .setTimestamp();
 
     const embed4 = new MessageEmbed()
     .setColor("WHITE")
     .setTitle("🕌    SAHURRRRRRRR!!     🕌")
-    .setDescription("**Good Morning Y'all !! | ||@everyone|| | SAHUR**\n Hari pertama puasa nih, Yok bangun yok!!\n> Waktu sahurnya masih ada sekitar `30 menit` lagi guys! Jangan sampe telat yaa! :hourglass: \n")
+    .setDescription("**Good Morning Y'all !! | ||<@614645570044166174>|| | SAHUR**\n Udah waktunya sahur nih, Yok bangun yok!!\n> Waktu sahurnya masih ada sekitar `30 menit` lagi, Jangan sampe telat yaa! :hourglass: \n")
     .setFooter({text: `Buruan makan, entar siang laper nangis lagi`, iconURL: `${client.user.displayAvatarURL({dynamic: true, size: 512})}`})
     .setTimestamp();
     
     const embed5 = new MessageEmbed()
     .setColor("WHITE")
     .setTitle("🕌    SAHURRRRRRRR!!     🕌")
-    .setDescription("**Dikit Lagi Imsak !! | ||@everyone|| | SAHUR**\n Hari pertama puasa nih, masa udah skip sahur sih? :v\n> Waktu menuju imsak tinggal `10 menit` lagi, Yang belom makan ayo makan! :hourglass: \n")
-    .setFooter({text: `Buruan makan, entar siang laper nangis lagi`, iconURL: `${client.user.displayAvatarURL({dynamic: true, size: 512})}`})
+    .setDescription("**Dikit Lagi Imsak !! | ||<@614645570044166174>|| | SAHUR**\n Dikit lagi imsak, yahhahah hayyuuu \n> Waktu menuju imsak tinggal `10 menit` lagi, Yang belom makan ayo makan! :hourglass: \n")
+    .setFooter({text: `Lupa di update kemaren, untung diingetin Sandwichs :v`, iconURL: `${client.user.displayAvatarURL({dynamic: true, size: 512})}`})
     .setTimestamp();
 
     const embed6 = new MessageEmbed()
     .setColor("WHITE")
     .setTitle("🕌    SAHURRRRRRRR!!     🕌")
-    .setDescription("**Selamat Berpuasa !! | ||@everyone|| | IMSAK**\n Waktu sahur sudah habis! Selamat untuk kalian yang sudah sempat makan, yang belum harap bersabar :v  \n")
+    .setDescription("**Selamat Berpuasa !! | ||<@614645570044166174>|| | IMSAK**\n Waktu sahur sudah habis! Selamat untuk kalian yang sudah sempat makan, yang belum harap bersabar :v  \n")
     .setFooter({text: `MEMASUKI WAKTU IMSAK`, iconURL: `${client.user.displayAvatarURL({dynamic: true, size: 512})}`})
     .setTimestamp();
 
-    cron.schedule('20 22 * * *', () => {
+    cron.schedule('29 02 * * *', () => {
         channel.send({embeds: [embed1]}).then((m) => {
           m.react("🤍")
 
           function countdown() {
             setTimeout(() => {
-              channel.send('@everyone').then((e) => {
+              channel.send('<@614645570044166174>').then((e) => {
                 e.delete()
               })
             }, ms('1 seconds'));
 
             setTimeout(() => {
-              channel.send('@everyone').then((e) => {
+              channel.send('<@614645570044166174>').then((e) => {
                 e.delete()
               })
             }, ms('2 seconds'));
 
             setTimeout(() => {
-              channel.send('@everyone').then((e) => {
+              channel.send('<@614645570044166174>').then((e) => {
                 e.delete()
               })
             }, ms('3 seconds'));
@@ -90,7 +90,7 @@ client.on("ready", () => {
             }, ms('110 minutes'));
 
             setTimeout(() => {
-              channel.send('@everyone').then((e) => {
+              channel.send('<@614645570044166174>').then((e) => {
                 e.delete()
               })
             }, ms('110 minutes'));
@@ -106,7 +106,7 @@ client.on("ready", () => {
             }, ms('2 hrs'));
 
             setTimeout(() => {
-              channel.send('@everyone').then((e) => {
+              channel.send('<@614645570044166174>').then((e) => {
                 e.delete()
               })
             }, ms('2 hours'));
